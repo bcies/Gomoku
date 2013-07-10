21979ed9b40f0543e95844154925fef4cf948373
Candice Schumann <schumannc1@student.lasalle.edu>
2013-07-10 14:18:59 -0700

    two players can now play against each other

--- src/gomoku_main/Gomoku.java
+++ src/gomoku_main/Gomoku.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-
 public class Gomoku {
 
 	private static Board board;
@@ -13,12 +12,13 @@ public class Gomoku {
 	public static void main(String args[]) {
 		board = new Board();
 		List<Double> turnTimes = new LinkedList<Double>();
-		Player player = new Player(2000);
+		Player black = new Player(20000);
+		Player white = new Player(2000);
 		String command;
 		StringTokenizer token;
 		Scanner in = new Scanner(System.in);
 		while (!board.hasWinner()) {
-//			command = in.nextLine();
+			// command = in.nextLine();
 			command = "genmove";
 			if (command.contains("showboard")) {
 				System.out.println(board);
@@ -38,13 +38,21 @@ public class Gomoku {
 					System.out.println("Invalid move.\n");
 				}
 			} else if (command.contains("genmove")) {
+				int playerMove;
 				long startTime = System.nanoTime();
-				int playerMove = player.getBestMove(board, command.contains("showtree"));
-				double turnTime = (System.nanoTime()-startTime)/1000000.0;
+				if (board.getColorToPlay() == Board.BLACK) {
+					playerMove = black.getBestMove(board,
+							command.contains("showtree"));
+				}
+				else {
+					playerMove = white.getBestMove(board,
+							command.contains("showtree"));
+				}
+				double turnTime = (System.nanoTime() - startTime) / 1000000.0;
 				turnTimes.add(turnTime);
 				System.out.println("Player played at "
 						+ board.indexToString(playerMove));
-				System.out.println("Player took " + (int)turnTime + "ms");
+				System.out.println("Player took " + (int) turnTime + "ms");
 				board.play(playerMove);
 			} else if (command.contains("quit")) {
 				System.exit(0);
@@ -55,14 +63,14 @@ public class Gomoku {
 			System.out.println("Black Wins!");
 		} else if (board.getWinner() == Board.WHITE) {
 			System.out.println("White Wins!");
-		}
-		else{
+		} else {
 			System.out.println("It's a tie");
 		}
 		double sum = 0;
-		for (Double time : turnTimes){
+		for (Double time : turnTimes) {
 			sum += time;
 		}
-		System.out.println("Average time taken: " + (int)(sum/turnTimes.size()) + "ms");
+		System.out.println("Average time taken: "
+				+ (int) (sum / turnTimes.size()) + "ms");
 	}
 }
