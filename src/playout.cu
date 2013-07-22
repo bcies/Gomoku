722bd7d23517d66116f1127a56690ef170e36600
bcieslak <bcieslak@lclark.edu>
2013-07-22 12:39:04 -0700

    Made comment changes to the cuda file

--- src/playout.cu
+++ src/playout.cu
@@ -16,10 +16,8 @@ __global__ void playout(int *rands, int *numRands, int *board, int *boardWidth, 
 //	int wincolor = -1;
 //	int colorTP = *colorToPlay;
 //
-//	int count = 0;
 //
 //	//actual playouts
-//	while (count < 1000){
 //		int n = rands[threadIdx.x % *numRands];
 //		if (tempBoard[n] == 0){
 //			tempBoard[n] = colorTP;
@@ -33,86 +31,7 @@ __global__ void playout(int *rands, int *numRands, int *board, int *boardWidth, 
 //
 //		//check end of game.....
 //
-//		int color;
-//		int win = 0;
+
 //
-//		int i;
-//		for (int x = 0; x < *boardWidth; x++){
-//			for (int y = 0; y < *boardWidth; y++){
-//				color = tempBoard[x + y * (*boardWidth)];
-//				if (color != 0){
-//					win = 1;
-//					if ((x >= 4) && (y<= *boardWidth -5)){
-//						for (i = 1; i < 5; i++){
-//							if (tempBoard[(x-i)+(y+i) * (*boardWidth)] != color){
-//								win = 0;
-//
-//								break;
-//							}
-//						}
-//						if (win){
-//							wincolor = color;
-//							break;
-//						}
-//					}
-//					win = 1;
-//					if (x <= *boardWidth -5 && y <= *boardWidth-5){
-//						for (i = 1; i < 5; i++){
-//							if (tempBoard[(x+i) + (y+i) * (*boardWidth)] != color){
-//								win = 0;
-//								break;
-//							}
-//						}
-//						if (win){
-//							wincolor = color;
-//							break;
-//						}
-//					}
-//					win = 1;
-//					if (y <= *boardWidth-5){
-//						for (i = 1; i < 5; i++){
-//							if (tempBoard[x + (y+i) * (*boardWidth)] != color){
-//								win = 0;
-//								break;
-//							}
-//						}
-//						if (win){
-//							wincolor = color;
-//							break;
-//						}
-//					}
-//					win = 1;
-//					if (x <= *boardWidth-5){
-//						for (i = 1; i < 5; i++){
-//							if (tempBoard[x+i+y * (*boardWidth)] != color){
-//								win = false;
-//								break;
-//							}
-//						}
-//						if (win){
-//							wincolor = color;
-//							break;
-//						}
-//					}
-//				} else {
-//					boardFull = 0;
-//				}
-//			}
-//			if (wincolor != -1){
-//				break;
-//			}
-//		}
-//		if (wincolor != -1){
-//			break;
-//		}
-//		count++;
-//	}
-//	if (boardFull) {
-//		atomicAdd(wins, (float)0.5);
-//	}
-//	else{
-//		if (wincolor != -1 && wincolor == colorTP){
 //			atomicAdd(wins, (float)1.0);
-//		}
-//	}
 }
